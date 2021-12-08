63ff78c0dda5a4ffac96a1ba099c0efd734389f3
chugogo <ioueasonae86@yahoo.com.tw>
2021-12-08 18:16:29 +0800

    Initial Commit

---
+++ UIPath製圖/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIPath製圖
+//
+//  Created by Chu Go-Go on 2021/12/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIPath製圖/UIPath__App.swift
@@ -0,0 +1,17 @@
+//
+//  UIPath__App.swift
+//  UIPath製圖
+//
+//  Created by Chu Go-Go on 2021/12/8.
+//
+
+import SwiftUI
+
+@main
+struct UIPath__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
